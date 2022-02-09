0ff0be7b29c33b3155c5cb64ab6860245fd4c352
memelopez <meme30lopez@gmail.com>
2022-02-09 00:38:58 -0600

    Add methods to make first connection with API. Don't wirk yet.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
 // src/index.js
 import './styles.css';
+import Store from './modules/store';
+
+// variables
+let pokemonList = [];
+let appID;
+const newAppURL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/`;
+
+
+appID = Store.getAppID();
+
+// methods
+
+const newAppID = async (url) => {
+  const newAppURL = `${url}/apps/`;
+  const response = await fetch(newAppURL, {
+    method: 'POST',
+    mode: 'CORS',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+  return data;
+};
+
+const loadAppID = async (url) => {
+  appID = await newAppID(url);
+  console.log('-->index, loadAppID: ', appID);
+};
+
+const initPokeApp = () => {
+  if (appID) { // appID is in localStorage
+    console.log('-->index, initPoke - appId is in localStorage');
+  } else { // appID IS NOT in localStorage
+    loadAppID(newAppURL);
+  }
+};
+
+// Events
+
+// When content loads
+document.addEventListener('DOMContentLoaded', initPokeApp());
